02a3e5bf9fa7358d8d525dbffc247827a1bdcac3
Mika Tuupola <tuupola@appelsiini.net>
2022-05-04 16:06:26 +0700

    Apply default code formatting
    $ mix format

--- test/branca_test.exs
+++ test/branca_test.exs
@@ -1,12 +1,15 @@
 defmodule BrancaTest do
   use ExUnit.Case
-  #doctest Branca, except: [:moduledoc, encode: 1, encode: 2, encode: 3]
+  # doctest Branca, except: [:moduledoc, encode: 1, encode: 2, encode: 3]
 
   @token "875GH233T7IYrxtgXxlQBYiFobZMQdHAT51vChKsAIYCFxZtL1evV54vYqLyZtQ0ekPHt8kJHQp0a"
   @forged "87x8H26DuLJfzToSrJwLLUnritpp2nUQe2bgoiUbx3wGWGbfNibpJOLqNqjvfsIMkKnqClPeJtFp6"
   @version "8AenkwdktKeVVe9e6uIEi0GwL72HMrPF3cFRsqF2DtxJYb0EZ1jDxdu40XgLY6swS9HleC2oAwvn4"
 
-  @nonce String.duplicate(<<0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c>>, 2)
+  @nonce String.duplicate(
+           <<0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C>>,
+           2
+         )
   @payload "Hello world!"
 
   test "Should encode payload" do
@@ -18,23 +21,23 @@ defmodule BrancaTest do
   end
 
   test "Should encode payload and timestamp" do
-    {:ok, token} = Branca.encode(@payload, timestamp: 123206400)
+    {:ok, token} = Branca.encode(@payload, timestamp: 123_206_400)
     assert Branca.decode(token) == {:ok, @payload}
 
-    token = Branca.encode!(@payload, timestamp: 123206400)
+    token = Branca.encode!(@payload, timestamp: 123_206_400)
     assert Branca.decode!(token) == @payload
   end
 
   test "Should encode and decode payload, timestamp and nonce" do
-    assert Branca.encode(@payload, timestamp: 123206400, nonce: @nonce) == {:ok, @token}
+    assert Branca.encode(@payload, timestamp: 123_206_400, nonce: @nonce) == {:ok, @token}
     assert Branca.decode(@token) == {:ok, @payload}
 
-    assert Branca.encode!(@payload, timestamp: 123206400, nonce: @nonce) == @token
+    assert Branca.encode!(@payload, timestamp: 123_206_400, nonce: @nonce) == @token
     assert Branca.decode!(@token) == @payload
   end
 
   test "Should fail with expired" do
-    assert Branca.encode(@payload, timestamp: 123206400, nonce: @nonce) == {:ok, @token}
+    assert Branca.encode(@payload, timestamp: 123_206_400, nonce: @nonce) == {:ok, @token}
     assert Branca.decode(@token, %{:ttl => 3600}) == {:error, :expired}
 
     assert_raise RuntimeError, "Token is expired.", fn ->
@@ -50,6 +53,7 @@ defmodule BrancaTest do
 
   test "Should fail with forged token" do
     assert Branca.decode(@forged, nonce: @nonce) == {:error, :forged}
+
     assert_raise RuntimeError, "Invalid token.", fn ->
       Branca.decode!(@forged, nonce: @nonce) == {:error, :forged}
     end
@@ -57,6 +61,7 @@ defmodule BrancaTest do
 
   test "Should fail with wrong wrong version" do
     assert Branca.decode(@version) == {:error, :unknown_version}
+
     assert_raise RuntimeError, "Unknown token version.", fn ->
       Branca.decode!(@version) == {:error, :unknown_version}
     end
@@ -64,6 +69,7 @@ defmodule BrancaTest do
 
   test "Should fail with invalid nonce" do
     assert Branca.encode(@payload, nonce: "invalid") == {:error, :invalid_argument}
+
     assert_raise RuntimeError, "Invalid arguments passed to Libsodium.", fn ->
       Branca.encode!(@payload, nonce: "invalid") == {:error, :invalid_argument}
     end
